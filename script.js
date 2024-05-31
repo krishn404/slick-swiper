b3a09009e7d427f22ca1b4df9c6bdc8fc116a720
krishn404 <maharshikrishnakant@gmail.com>
2024-05-31 17:40:14 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    $('.testimonial-slider').slick({
+        infinite: true,
+        slidesToShow: 6, // Change the number of slides to show to 4
+        slidesToScroll: 2,
+        autoplay: true,
+        autoplaySpeed: 500000,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+const responsiveSwiper = new Swiper('.swiper-container', {
+  breakpoints: {
+      0: {
+          slidesPerView: 1,
+          spaceBetween: 20
+      },
+      768: {
+          slidesPerView: 2,
+          spaceBetween: 30
+      },
+      1024: {
+          slidesPerView: 4,
+          spaceBetween: 40
+      }
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var descriptions = document.querySelectorAll('.description');
+  descriptions.forEach(function(description) {
+      var lastWord = description.textContent.trim().split(/\s+/).pop();
+      var lastWordPosition = lastWord.length;
+      var closingQuote = description.parentElement.querySelector('.description::after');
+      closingQuote.style.left = 'calc(' + lastWordPosition + 'ch + 20px)';
+  });
+});
